c9e0bebec4dc2b05036a72e61b5a2edc88da1d90
Nathachai Thongniran <inid3a@gmail.com>
2017-10-04 07:22:29 +0700

    Add package script

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const zip = require('gulp-zip');
+const clean = require('gulp-clean');
+const fs = require('fs');
+
+const mainFile = 'mourning.php';
+const mainFileContent = fs.readFileSync(mainFile, 'utf8');
+const pluginVersion = /^Version.*$/igm.exec(mainFileContent)[0].substring(9).trim();
+const pluginName = /^Plugin Name.*$/igm.exec(mainFileContent)[0].substring(13).trim().toLowerCase();
+const packageFolderName = pluginName + '-' + pluginVersion;
+
+gulp.task('clean', function () {
+  return gulp.src(pluginName + '-*', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('pack', ['clean'], function () {
+  return gulp.src([
+    'css/**',
+    'image/**',
+    'mourning.php',
+    'readme.txt',
+    'screenshot-1.jpg',
+    'screenshot-2.jpg'
+  ], {base: '.'})
+    .pipe(gulp.dest(packageFolderName));
+});
+
+gulp.task('pack.zip', ['pack'], function () {
+  return gulp.src(packageFolderName + '/**')
+    .pipe(zip(packageFolderName + '.zip'))
+    .pipe(gulp.dest('./'));
+});
